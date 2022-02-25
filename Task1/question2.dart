2df738d1b1cf92f1568a01f242ae9ee496870527
devanshi05chaturvedi <80825634+devanshi05chaturvedi@users.noreply.github.com>
2022-02-25 21:09:09 +0530

    Create question2.dart

---
+++ Task1/question2.dart
@@ -0,0 +1,13 @@
+//2. WAP to check if a number is even or odd using functions.
+void cal()
+{
+  int n=9;
+  if (n%2==0)
+    print("Even number");
+  else
+    print("Odd number");;
+}
+void main()
+{
+  cal();
+}
