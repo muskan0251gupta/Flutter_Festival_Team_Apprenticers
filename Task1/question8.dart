da6d3cf395450ddc3d6261dd9e7a2d7cce848ffc
devanshi05chaturvedi <80825634+devanshi05chaturvedi@users.noreply.github.com>
2022-02-25 21:14:52 +0530

    Create question8.dart

---
+++ Task1/question8.dart
@@ -0,0 +1,8 @@
+//8. Write a program to find the maximum between three numbers using the ternary operator.
+
+void main()
+{
+  int a=5,b=9,c=1,max;
+  max=(a>b?(a>c?a:c):(b>c?b:c));
+  	print("MAXIMUM OF THREE NUMBERS IS $max");
+}
